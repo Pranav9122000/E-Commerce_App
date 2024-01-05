9f8b21c5266074d8a5cabe5e9e25fcaacd32758c
Pranav Kulkarni <pranav.kulkarni@sap.com>
2024-01-05 19:19:32 +0530

    Initial Commit

---
+++ E-Commerce/E_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp.swift
+//  E-Commerce
+//
+//  Created by Kulkarni, Pranav on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
